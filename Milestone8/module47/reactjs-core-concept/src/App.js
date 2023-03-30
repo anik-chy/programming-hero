7647c44ae070f6519552c9af2ec51054110d36fd
anik-chy <anikchy2140@gmail.com>
2023-03-30 23:19:25 +0600

    module48 added

--- Milestone8/module47/reactjs-core-concept/src/App.js
+++ Milestone8/module47/reactjs-core-concept/src/App.js
@@ -2,24 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const cart = [
+    {name:"Anik", price:10},
+    {name:"Bnik", price:20},
+    {name:"Cnik", price:30},
+    {name:"Dnik", price:40},
+    {name:"Enik", price:50},
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        cart.map(product => <Product name={product.name} price={product.price}></Product>)
+      }
     </div>
   );
+}
+
+function Product(props){
+  return (
+    <div className='product-container'>
+      
+      <h2>Name: {props.name}</h2>
+      <h4>Price: {props.price} BDT</h4>
+    </div>
+  )
 }
 
 export default App;
